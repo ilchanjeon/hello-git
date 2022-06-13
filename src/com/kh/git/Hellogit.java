caccf9d58ab9715d96f99e9fb4e5f2e7ddbf89e1
ilchan <winters63@naver.com>
2022-06-13 17:38:39 +0900

    dog 기능 완성!

--- src/com/kh/git/Hellogit.java
+++ src/com/kh/git/Hellogit.java
@@ -1,5 +1,13 @@
 package com.kh.git;
 
-public class Hellogit {
+import com.kh.git.animal.Dog;
 
+public class Hellogit {
+	public static void main(String[] args) {
+		System.out.println("hello git");
+		
+		// dog 기능
+		Dog dog = new Dog();
+		dog.bark();
+	}
 }
